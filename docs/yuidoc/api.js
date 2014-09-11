aae01dc5e3b5a829f80e77a2a2e70d5f5067e6ff
Yuki Asano <y.asano0123@gmail.com>
2014-09-12 00:00:23 +0900

    docs

---
+++ docs/yuidoc/api.js
@@ -0,0 +1,47 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Modules_Abstract",
+        "Modules_Contacts",
+        "Modules_Me",
+        "Modules_My",
+        "Modules_Rooms",
+        "Modules_Rooms_Room",
+        "PW"
+    ],
+    "modules": [
+        "Modules_Contacts",
+        "Modules_Me",
+        "Modules_My",
+        "Modules_Rooms",
+        "Modules_Rooms_Room"
+    ],
+    "allModules": [
+        {
+            "displayName": "Modules_Contacts",
+            "name": "Modules_Contacts",
+            "description": "コンタクトユーザモジュール"
+        },
+        {
+            "displayName": "Modules_Me",
+            "name": "Modules_Me",
+            "description": "自分自身の情報モジュール"
+        },
+        {
+            "displayName": "Modules_My",
+            "name": "Modules_My",
+            "description": "自分が持つデータへのアクセスモジュール"
+        },
+        {
+            "displayName": "Modules_Rooms",
+            "name": "Modules_Rooms",
+            "description": "チャットルーム管理モジュール"
+        },
+        {
+            "displayName": "Modules_Rooms_Room",
+            "name": "Modules_Rooms_Room",
+            "description": "チャットルームモジュール"
+        }
+    ]
+} };
+});
